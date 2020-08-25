558a815ccd5a6e015010c28e115b59a3f6e6f156
pgaliardi <galiardi.dev@gmail.com>
2020-08-25 00:57:40 -0400

    firebaseConfig oculto

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,7 @@ import firebase from 'firebase/app';
 //import 'firebase/firestore';
 import 'firebase/auth';
 //import 'firebase/firebase-analytics';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA8buaiwN0HPDhkHIqXAKrzwMz6Zo0Easo",
-  authDomain: "mercadito-ddfb3.firebaseapp.com",
-  databaseURL: "https://mercadito-ddfb3.firebaseio.com",
-  projectId: "mercadito-ddfb3",
-  storageBucket: "mercadito-ddfb3.appspot.com",
-  messagingSenderId: "507056990569",
-  appId: "1:507056990569:web:834c865d378ac48c15b5f8",
-  measurementId: "G-52H3CWQJ6X"
-};
+import { firebaseConfig } from './firebaseConfig';
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
 
